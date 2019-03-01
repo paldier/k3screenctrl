480200ca674977c1607d34111eb7822ec48f87d2
paldier <paldier@hotmail.com>
2019-03-02 00:13:40 +0800

    优化脚本

--- lib/k3screenctrl/weather.sh
+++ lib/k3screenctrl/weather.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-function get_json_value()
+get_json_value()
 {
   local json=$1
   local key=$2
@@ -9,22 +8,31 @@ function get_json_value()
   else
     local num=$3
   fi
-
-  local value=$(echo "${json}" | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'${key}'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p)
-
+  local value=$(echo "${json}" | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'${key}'"/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p)
   echo ${value}
 }
 week=$(date +%w)
 data=$(date +%Y-%m-%d)
 time=$(date +%H:%m)
-get_json_value "$(curl -s http://ip.taobao.com/service/getIpInfo.php?ip=myip)" city > /tmp/city
-iconv -f gb2312 -t UTF-8 -c /tmp/city
-city=`cat /tmp/city`
-
+[ -e "/lib/k3screenctrl/time" ] || echo $(($(date +%s)+7200)) > /lib/k3screenctrl/time
+starttime=`cat /lib/k3screenctrl/time`
+[ ! -f "/lib/k3screenctrl/http" -o $(date +%s) -ge $starttime ] && json=`curl -s "https://api.seniverse.com/v3/weather/now.json?key=xxxxxxxxxxxxxx&location=ip&language=zh-Hans&unit=c"` && echo $json > /lib/k3screenctrl/http && echo $(($(date +%s)+7200)) > /lib/k3screenctrl/time
+#set your key
+if [ -n "$(echo $json | grep AP010003)" ]; then
+city=北京
+temp=11
+code=0
+else
+http=`cat /lib/k3screenctrl/http`
+city=`get_json_value "$http" name`
+temp=`get_json_value "$http" temperature`
+code=`get_json_value "$http" code`
+fi
 echo $city
-echo 11
+echo $temp
 echo $data
 echo $time
-echo 3
+echo $code
 echo $week
 echo 0
+
